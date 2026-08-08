58bcb3b9798722a96cfba6edae9bc64ee99634fa
shantaurus <61095048+shantaurus@users.noreply.github.com>
2026-08-09 01:03:23 +0530

    Update password

--- 2025/Prod/provisioners/scripts/bootstrap_winrm.ps1
+++ 2025/Prod/provisioners/scripts/bootstrap_winrm.ps1
@@ -1,13 +1,17 @@
 <powershell>
-# Enable WinRM HTTP Listener & Allow Unencrypted Auth (for Packer setup)
+# 1. Set local Administrator password to match your Packer variable
+$admin = [adsi]"WinNT://./Administrator,user"
+$admin.SetPassword("YourKnownAdminPassword123!")
+
+# 2. Quickconfig & Enable Unencrypted Basic Auth for WinRM
 winrm quickconfig -q
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 
-# Open Windows Firewall for WinRM HTTP (5985)
+# 3. Open Firewall Port 5985
 netsh advfirewall firewall set rule group="remote administration" new enable=yes
 netsh advfirewall firewall add rule name="WinRM 5985" dir=in action=allow protocol=TCP localport=5985
 
-# Set Execution Policy
+# 4. Set Execution Policy
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
 </powershell>
