f359003f6107aa75503a1bddbbead2fa03f38dad
shantaurus <61095048+shantaurus@users.noreply.github.com>
2026-08-11 14:05:19 +0530

    Update

--- 2025/Prod/provisioners/scripts/bootstrap_winrm.ps1
+++ 2025/Prod/provisioners/scripts/bootstrap_winrm.ps1
@@ -7,6 +7,7 @@ $admin.SetPassword("YourKnownAdminPassword123!")
 winrm quickconfig -q
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
 
 # 3. Open Firewall Port 5985
 netsh advfirewall firewall set rule group="remote administration" new enable=yes
@@ -14,4 +15,11 @@ netsh advfirewall firewall add rule name="WinRM 5985" dir=in action=allow protoc
 
 # 4. Set Execution Policy
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+
+# 5. Prevent registry hive unload during long WinRM Ansible updates
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "System" -Force -ErrorAction SilentlyContinue
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "DisableForceUnload" -Value 1 -Type DWord
+
+# 6. Restart WinRM service to apply configuration updates
+Restart-Service WinRM
 </powershell>
